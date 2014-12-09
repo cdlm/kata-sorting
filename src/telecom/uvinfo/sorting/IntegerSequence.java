a00c5b360dfedb0f669c3393f4a900325585b825
Damien Pollet <damien.pollet@gmail.com>
2014-12-09 17:41:53 +0100

    Reorganize & add markup to hide exercise code

--- src/telecom/uvinfo/sorting/IntegerSequence.java
+++ src/telecom/uvinfo/sorting/IntegerSequence.java
@@ -7,15 +7,24 @@ public class IntegerSequence {
 	private int[] storage, tmp; // tmp is used only during mergeSort()
 	private int size;
 
+	/**
+	 * Construit une séquence <i>vide</i>.
+	 * 
+	 * @param capacity
+	 *            Nombre maximum d'éléments pouvant être ajoutés dans la
+	 *            séquence.
+	 */
 	public IntegerSequence(int capacity) {
 		storage = new int[capacity];
 		size = 0;
 	}
 
+	/** @return Taille effective de la séquence (nombre d'éléments). */
 	public int size() {
 		return size;
 	}
 
+	/** @return Espace total disponible (nombre maximum d'éléments). */
 	public int capacity() {
 		return storage.length;
 	}
@@ -28,94 +37,23 @@ public class IntegerSequence {
 		return storage[index];
 	}
 
-	public void addIncreasingIntegers(int howMany) {
-		for (int i = 1; i <= howMany; i++)
-			this.addElement(i);
-	}
-
-	public void addDecreasingIntegers(int howMany) {
-		for (int i = howMany; i >= 1; i--)
-			this.addElement(i);
-	}
-
-	/**
-	 * Mélange les éléments (selon l'<a
-	 * href="http://en.wikipedia.org/wiki/Fisher-Yates_shuffle" >algorithme
-	 * Fisher-Yates</a>)
-	 * 
-	 * @param seed
-	 *            Graine d'initialisation du générateur pseudo-aléatoire, pour
-	 *            pouvoir reproduire le mélange à l'identique.
-	 */
-	public void shuffle(long seed) {
-		Random random = new Random(seed);
-		for (int index = storage.length - 1; index > 0; index--) {
-			int other = random.nextInt(index + 1); // other takes a value in
-													// 0..index
-			this.swap(index, other);
-		}
-	}
-
-	/**
-	 * Mélange <i>partiellement</i> les éléments, en conservant donc
-	 * approximativement leur ordre initial.
-	 * 
-	 * @param seed
-	 *            Graine du générateur pseudo-aléatoire.
-	 * @param maxDistance
-	 *            Distance maximale des échanges d'éléments ; valeurs possibles
-	 *            entre 0 (mélange identité) et la taille des données (mélange
-	 *            de Fisher-Yates).
-	 */
-	public void localizedShuffle(long seed, int maxDistance) {
-		Random random = new Random(seed);
-		for (int index = storage.length - 1; index > 0; index--) {
-			int pickDistance = Math.min(index, maxDistance);
-			int other = index - random.nextInt(pickDistance + 1);
-			this.swap(index, other);
-		}
-	}
-
-	/**
-	 * Échange les éléments stockés aux indices donnés.
-	 * 
-	 * @param index
-	 * @param other
-	 */
-	public void swap(int index, int other) {
-		int tmp = storage[other];
-		storage[other] = storage[index];
-		storage[index] = tmp;
-	}
-
-	/**
-	 * Détermine si la séquence est triée.
-	 * 
-	 * @return true si les éléments sont en ordre croissant.
-	 */
-	public boolean isSorted() {
-		if (size == 0)
-			return true;
-
-		int prev = storage[0];
-		for (int x : storage) {
-			if (x < prev)
-				return false;
-			prev = x;
-		}
-
-		return true;
-	}
-
+	/** Trie la séquence par tri-fusion. */
 	public void mergeSort() {
 		tmp = new int[storage.length];
+		// {{{ task implémenter le tri fusion
 		mergeSort(0, size - 1);
+		// }}}
 		tmp = null; // tmp is now wasted space, throw it away
 	}
 
+	/** Trie la séquence par tri-insertion. */
 	public void insertSort() {
+		// {{{ task implémenter le tri insertion
 		insertSort(size);
+		// }}}
 	}
+
+	// {{{
 
 	public void mergeSort(int fromIndex, int toIndex) {
 		if (fromIndex < toIndex) {
@@ -157,13 +95,98 @@ public class IntegerSequence {
 		}
 	}
 
+	// }}}
+
+	/*
+	 * Les méthodes suivantes sont fournies pour faciliter la construction des
+	 * données lors des mesures. Voir <code>IntegerSequenceTest</code> pour des
+	 * exemples d'utilisation.
+	 */
+
+	public void addIncreasingIntegers(int howMany) {
+		for (int i = 1; i <= howMany; i++)
+			this.addElement(i);
+	}
+
+	public void addDecreasingIntegers(int howMany) {
+		for (int i = howMany; i >= 1; i--)
+			this.addElement(i);
+	}
+
+	/**
+	 * Mélange les éléments (selon l'<a
+	 * href="http://en.wikipedia.org/wiki/Fisher-Yates_shuffle" >algorithme
+	 * Fisher-Yates</a>)
+	 * 
+	 * @param seed
+	 *            Graine d'initialisation du générateur pseudo-aléatoire, pour
+	 *            pouvoir reproduire le mélange à l'identique.
+	 */
+	public void shuffle(long seed) {
+		Random random = new Random(seed);
+		for (int index = storage.length - 1; index > 0; index--) {
+			// other takes a value in 0..index
+			int other = random.nextInt(index + 1);
+			this.swap(index, other);
+		}
+	}
+
+	/**
+	 * Mélange <i>partiellement</i> les éléments, en conservant donc
+	 * approximativement leur ordre initial.
+	 * 
+	 * @param seed
+	 *            Graine du générateur pseudo-aléatoire.
+	 * @param maxDistance
+	 *            Distance maximale des échanges d'éléments ; valeurs possibles
+	 *            entre 0 (mélange identité) et la taille des données (mélange
+	 *            de Fisher-Yates).
+	 */
+	public void localizedShuffle(long seed, int maxDistance) {
+		Random random = new Random(seed);
+		for (int index = storage.length - 1; index > 0; index--) {
+			int pickDistance = Math.min(index, maxDistance);
+			int other = index - random.nextInt(pickDistance + 1);
+			this.swap(index, other);
+		}
+	}
+
+	/**
+	 * Échange deux éléments de la séquence.
+	 * 
+	 * @param index Indice d'un élément.
+	 * @param other Indice de l'autre élément.
+	 */
+	public void swap(int index, int other) {
+		int tmp = storage[other];
+		storage[other] = storage[index];
+		storage[index] = tmp;
+	}
+
+	/**
+	 * Détermine si la séquence est triée.
+	 * 
+	 * @return <code>true</code> si les éléments sont en ordre croissant.
+	 */
+	public boolean isSorted() {
+		if (size == 0)
+			return true;
+		int prev = storage[0];
+		for (int x : storage) {
+			if (x < prev)
+				return false;
+			prev = x;
+		}
+		return true;
+	}
+
 	public IntegerSequence copy() {
 		IntegerSequence result = new IntegerSequence(size);
 		for (int index = 0; index < size; index++)
 			result.addElement(storage[index]);
 		return result;
 	}
-	
+
 	public String toString() {
 		StringBuffer sb = new StringBuffer(2 * size + 1);
 		sb.append("[");
